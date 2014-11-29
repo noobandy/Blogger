780901695c7a86284ea50b4f6901fb923f8aa568
noobandy <noobandy1364@gmail.com>
2014-11-29 17:24:07 +0530

    Navbar

--- app/views/index.php
+++ app/views/index.php
@@ -12,11 +12,29 @@
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
  		<div class="container">
- 			<form class="navbar-form navbar-right" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search">
-				</div>
-			</form>
+ 			<!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Blogger</a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="#">About <span class="sr-only">(current)</span></a></li>
+		      </ul>
+		      <form class="navbar-form navbar-right" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+			  </form>
+
+    </div><!-- /.navbar-collapse -->
  		</div>
 	</nav>
 	<div class="container">
